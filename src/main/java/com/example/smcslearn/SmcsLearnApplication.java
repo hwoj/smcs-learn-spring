d02376a3b1644b0dfce59d3e88110b4abe7c48c6
hwoj <hwojnicki41@gmail.com>
2020-02-12 10:15:01 -0500

    initial commit

---
+++ src/main/java/com/example/smcslearn/SmcsLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.smcslearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmcsLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmcsLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smcslearn/SmcsLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smcslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmcsLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
